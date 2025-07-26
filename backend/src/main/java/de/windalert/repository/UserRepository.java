f7777198f286b0859e6f36c1e4142c4b6952925b
Liam David Boddin <stu222452@mail.uni-kiel.de>
2025-07-26 21:09:41 +0200

    added user repo

---
+++ backend/src/main/java/de/windalert/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package de.windalert.repository;
+
+import de.windalert.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String email);
+    Optional<User> findByActivationToken(String token);
+}
